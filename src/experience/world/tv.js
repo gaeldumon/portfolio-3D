5b80e633b179c71662ab0e2bdd64d15f057d4b1d
gaeldumon <dmnleag@protonmail.com>
2023-12-22 10:07:33 +0100

    Start to change tv channel texture

--- src/experience/world/tv.js
+++ src/experience/world/tv.js
@@ -10,6 +10,13 @@ export default class Tv {
         this.resource = this.resources.items.gltfTvModel.scene;
         this.time = this.experience.time;
 
+        this.tvChannelsTextures = [
+            this.resources.items.channelTextureAdnFamily,
+            this.resources.items.channelTextureInstantVital
+        ];
+
+        this.currentChannel = 0;
+
         this.debug = this.experience.debug;
         if (this.debug.active) {
             this.debugFolder = this.debug.ui.addFolder('tv');
@@ -17,11 +24,24 @@ export default class Tv {
 
         this.setModel();
 
+        this.changeChannel();
     }
 
     setModel() {
         this.model = this.resource;
         this.model.position.set(0, -1, 0);
         this.scene.add(this.model);
+    }
+
+    changeChannel() {
+        this.model.traverse((child) => {
+            if (
+                child.name === "Plane_Material006_0" &&
+                child instanceof THREE.Mesh &&
+                child.material.map instanceof THREE.Texture
+            ) {
+                child.material.map = this.tvChannelsTextures[this.currentChannel];
+            }
+        })
     }
 }
